801394652574def48afa953ce46e71b4499fc841
Darker <justdarker@gmail.com>
2020-02-01 20:31:29 +1000

    refactor1

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -9,10 +9,10 @@ def main():
 
 
 def right_answer(question):
-    list_of_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 
-                      41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 
+    list_of_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
+                      41, 43, 47, 53, 59, 61, 67, 71, 73, 79,
                       83, 89, 97, 101]
-    
+
     primes = set(list_of_primes)
 
     if question in primes:
